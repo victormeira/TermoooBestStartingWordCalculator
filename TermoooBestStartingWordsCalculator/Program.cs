3194f803fd4dc7ac30c2b0b9b61ed1f1969cae35
Victor Meira <victormeirapinto@icloud.com>
2022-02-03 00:59:13 -0300

    Added first version of Program.

---
+++ TermoooBestStartingWordsCalculator/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using WeCantSpell;
+using WeCantSpell.Hunspell;
+
+namespace TermoooBestStartingWordsCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> rankingOfScorePerFiveLetterWord = new Dictionary<string, double>();
+
+            List<string> fiveLetterWords = GenerateFiveLetterWordsDictionary();
+            List<string> termoooWords = new List<string>()
+            {
+                "motim",
+                "monta",
+                "tonta"
+            };
+
+            foreach(string word in fiveLetterWords)
+            {
+                rankingOfScorePerFiveLetterWord.Add(word, CalculateAverageTermoooScoreForWord(word, termoooWords));
+            }
+
+            Console.WriteLine("Best start words:");
+            foreach(var bestWordAndScore in rankingOfScorePerFiveLetterWord.OrderByDescending(i => i.Value).Take(20))
+            {
+                Console.WriteLine(bestWordAndScore.Key + " - " + bestWordAndScore.Value.ToString("#.##"));
+            }
+        }
+
+
+        static string GenerateNextWord(string currentWord)
+        {
+            string nextWord = "";
+            char[] reversedWord = currentWord.Reverse().ToArray();
+
+            bool shouldCarryOne = true;
+            for(int i = 0; i < currentWord.Length; i++)
+            {
+                char currentChar = reversedWord[i];
+
+                if (shouldCarryOne)
+                {
+                    if(currentChar == 'z')
+                    {
+                        currentChar = 'a';
+                        shouldCarryOne = true;
+                    }
+                    else
+                    {
+                        currentChar = (char) (currentChar + 1);
+                        shouldCarryOne = false;
+                    }
+                }
+
+                nextWord = currentChar + nextWord;
+            }
+
+            return nextWord;
+        }
+
+        static double CalculateAverageTermoooScoreForWord(string guessWord, List<string> listOfFiveLetterWords)
+        {
+            double totalScore          = 0;
+            int     totalProcessedWords = 0;
+
+            foreach(string possibleFiveLetterWord in listOfFiveLetterWords)
+            {
+                totalProcessedWords++;
+                totalScore += CalculateTermooScoreForWord(guessWord, possibleFiveLetterWord);
+            }
+
+            return totalScore / totalProcessedWords++;
+        }
+
+        static double CalculateTermooScoreForWord(string guessWord, string correctWord)
+        {
+            double totalScore = 0;
+            for(int i = 0; i < guessWord.Length; i++)
+            {
+                if(guessWord[i] == correctWord[i])
+                {
+                    totalScore += 1;
+                }
+                else if(correctWord.Contains(guessWord[i]))
+                {
+                    totalScore += 0.2;
+                }
+            }
+
+            return totalScore;
+        }
+
+        static List<string> GenerateFiveLetterWordsDictionary()
+        {
+            if (File.Exists("./Dicionario/PalavrasDe5Letras.txt"))
+            {
+                Console.WriteLine("Loaded five letter words from file.");
+                return File.ReadAllLines("./Dicionario/PalavrasDe5Letras.txt").ToList();
+            }
+
+            StreamWriter sw = new StreamWriter("./Dicionario/PalavrasDe5Letras.txt");
+
+            var dictionary = WordList.CreateFromFiles(@"./Dicionario/pt_BR.dic");
+            List<string> fiveLetterWords = new List<string>();
+
+            int totalProcessed = 0;
+            long totalPossibleWords = 26 * 26 * 26 * 26 * 26;
+            string currentWord = "aaaaa";
+            while (!currentWord.Equals("zzzzz"))
+            {
+                if (++totalProcessed % 10000 == 0)
+                {
+                    Console.WriteLine("Total Processed: " + (totalProcessed * 100.0 / totalPossibleWords).ToString("#.##") + "% and found a total of " + fiveLetterWords.Count + " words.");
+                }
+
+                if (dictionary.Check(currentWord))
+                {
+                    fiveLetterWords.Add(currentWord);
+                    sw.WriteLine(currentWord);
+                }
+
+                currentWord = GenerateNextWord(currentWord);
+            }
+            sw.Close();
+            return fiveLetterWords;
+        }
+    }
+}
